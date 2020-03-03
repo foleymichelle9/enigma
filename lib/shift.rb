104e5b01062fbd34f0565115de61db13294b9984
michelle <foleymichelle9@gmail.com>
2020-03-03 06:39:28 -0700

    write generate key method

--- lib/shift.rb
+++ lib/shift.rb
@@ -6,4 +6,13 @@ class Key
     @key = key
     @hash = {:A => "", :B => "", :C => "", :D => ""}
   end
+
+
+  def generate_key(key)
+    if key.length == 5
+      @key = key
+    else
+      @key = 5.times.map{Random.rand(10)}.join.to_s
+    end
+  end
 end
