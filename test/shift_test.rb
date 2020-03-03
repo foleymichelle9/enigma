2df17a5e13d10eba11ab754e5cac672393b8bf70
michelle <foleymichelle9@gmail.com>
2020-03-03 06:11:59 -0700

    First tets

---
+++ test/shift_test.rb
@@ -0,0 +1,10 @@
+require_relative 'test_helper'
+require './lib/shift'
+
+class ShiftTest < Minitest::Test
+
+  def test_it_exists
+    shift = Shift.new
+    assert_instance_of Shift, shift
+  end
+end
